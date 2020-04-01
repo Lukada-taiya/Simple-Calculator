aeb0bcfc3e18e11cba2102d45cc19b372078719b
Lukada-taiya <lukmanadam43@gmail.com>
2020-04-01 21:49:01 +0000

    Added functionality to the Switch Theme button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,13 +2,19 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
+
+    private int themeNum = 0;
 
     @FXML
     private Button btnScientific, btnTheme, btnC, btnBack, btnMod, btnDivide, btn7, btn8, btn9, btnMultiply, btn4, btn5, btn6, btnMinus,
     btn1, btn2, btn3, btnPlus, btnDot, btn0, btnEqual;
+
 
     @FXML
     private void onMouseEntered() {
@@ -38,7 +44,6 @@ public class Controller {
     @FXML
     private void onMouseClicked() {
         btnScientific.setCursor(Cursor.CLOSED_HAND);
-        btnTheme.setCursor(Cursor.CLOSED_HAND);
         btnC.setCursor(Cursor.CLOSED_HAND);
         btnBack.setCursor(Cursor.CLOSED_HAND);
         btnMod.setCursor(Cursor.CLOSED_HAND);
@@ -58,6 +63,93 @@ public class Controller {
         btnDot.setCursor(Cursor.CLOSED_HAND);
         btn0.setCursor(Cursor.CLOSED_HAND);
         btnEqual.setCursor(Cursor.CLOSED_HAND);
+    }
+
+    @FXML
+    public void switchTheme(MouseEvent event) {
+        btnTheme.setCursor(Cursor.CLOSED_HAND);
+
+        Parent root = ((Node) event.getSource()).getScene().getRoot();
+
+        themeNum++;
+        if(themeNum == 0) {
+            root.setStyle("-fx-background-color: #6f6e6e;");
+
+            btnScientific.setStyle("-fx-background-color: #f1ecec;");
+            btnTheme.setStyle("-fx-background-color: #f1ecec;");
+            btnC.setStyle("-fx-background-color: #f1ecec;");
+            btnBack.setStyle("-fx-background-color: #f1ecec;");
+            btnMod.setStyle("-fx-background-color: #f1ecec;");
+            btnDivide.setStyle("-fx-background-color: #f1ecec;");
+            btn7.setStyle("-fx-background-color: #f1ecec;");
+            btn8.setStyle("-fx-background-color: #f1ecec;");
+            btn9.setStyle("-fx-background-color: #f1ecec;");
+            btnMultiply.setStyle("-fx-background-color: #f1ecec;");
+            btn4.setStyle("-fx-background-color: #f1ecec;");
+            btn5.setStyle("-fx-background-color: #f1ecec;");
+            btn6.setStyle("-fx-background-color: #f1ecec;");
+            btnMinus.setStyle("-fx-background-color: #f1ecec;");
+            btn1.setStyle("-fx-background-color: #f1ecec;");
+            btn2.setStyle("-fx-background-color: #f1ecec;");
+            btn3.setStyle("-fx-background-color: #f1ecec;");
+            btnPlus.setStyle("-fx-background-color: #f1ecec;");
+            btnDot.setStyle("-fx-background-color: #f1ecec;");
+            btn0.setStyle("-fx-background-color: #f1ecec;");
+            btnEqual.setStyle("-fx-background-color: #f1ecec;");
+
+        }else if(themeNum == 1) {
+            root.setStyle("-fx-background-color: #f6f6f6;");
+
+            btnScientific.setStyle("-fx-background-color: #fff;");
+            btnTheme.setStyle("-fx-background-color: #fff;");
+            btnC.setStyle("-fx-background-color: #fff;");
+            btnBack.setStyle("-fx-background-color: #fff;");
+            btnMod.setStyle("-fx-background-color: #fff;");
+            btnDivide.setStyle("-fx-background-color: #fff;");
+            btn7.setStyle("-fx-background-color: #fff;");
+            btn8.setStyle("-fx-background-color: #fff;");
+            btn9.setStyle("-fx-background-color: #fff;");
+            btnMultiply.setStyle("-fx-background-color: #fff;");
+            btn4.setStyle("-fx-background-color: #fff;");
+            btn5.setStyle("-fx-background-color: #fff;");
+            btn6.setStyle("-fx-background-color: #fff;");
+            btnMinus.setStyle("-fx-background-color: #fff;");
+            btn1.setStyle("-fx-background-color: #fff;");
+            btn2.setStyle("-fx-background-color: #fff;");
+            btn3.setStyle("-fx-background-color: #fff;");
+            btnPlus.setStyle("-fx-background-color: #fff;");
+            btnDot.setStyle("-fx-background-color: #fff;");
+            btn0.setStyle("-fx-background-color: #fff;");
+            btnEqual.setStyle("-fx-background-color: #fff;");
+
+        }else if(themeNum == 2) {
+            root.setStyle("-fx-background-color: #5157ff;");
+
+            btnScientific.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnTheme.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnC.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnBack.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnMod.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnDivide.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn7.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn8.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn9.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnMultiply.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn4.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn5.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn6.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnMinus.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn1.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn2.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn3.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnPlus.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnDot.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btn0.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff");
+            btnEqual.setStyle("-fx-background-color: #0009FF; -fx-text-fill: #fff;");
+
+
+            themeNum = -1;
+        }
     }
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,13 +10,17 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Main Scene
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         root.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-        primaryStage.setTitle("Hello World");
+        root.setStyle("-fx-background-color: #6f6e6e;");
+        primaryStage.setTitle("Simple Calculator");
         primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(root, 300, 350));
+        primaryStage.setScene(new Scene(root, 300, 360));
         primaryStage.show();
+
     }
+
 
 
     public static void main(String[] args) {
